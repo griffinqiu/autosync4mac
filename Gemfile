0c23dab3494b8fabeb2740a902cb02f4fdd9ba30
Griffin Qiu <griffinqiu@gmail.com>
2016-08-05 06:49:27 +0000

    Init

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'http://ruby.taobao.org'
+
+gem 'listen'
+gem 'ruby-growl'
+gem 'net-scp'
---
+++ sync2.rb
@@ -0,0 +1,79 @@
+require 'rubygems'
+require 'net/scp'
+require 'ruby-growl'
+require 'listen'
+require 'yaml'
+
+LOCK_FILE = '.lock4autosync'
+
+if File.exist? LOCK_FILE
+  p 'sync process is already running! This process will exit.'
+  exit
+end
+
+YAML::ENGINE.yamler = 'syck'
+
+CONFIG = YAML::load(File.open("#{File.dirname(__FILE__)}/config.yml"))
+
+$host = CONFIG['host']
+$username = CONFIG['username']
+$password = CONFIG['password']
+$sco_src_dir = CONFIG['sco_src']
+$sco_dest_dir = CONFIG['sco_dest']
+
+
+$scp = Net::SCP.start($host, $username, :password => $password)
+$growl = nil
+
+
+#if Object.const_defined? :Growl
+  #class UUID
+    #def generate() 4 end
+  #end
+  #$growl = Growl::GNTP.new 'localhost', 'autosync4mac'
+  #$growl.uuid = UUID.new
+  #$growl.add_notification "autosync4mac", "Auto Sync 4 Mac Notification", "PNG", true
+  #$growl.register
+  #$growl.notify "autosync4mac", "welcome", "auto sync 4 mac is running now."
+#end
+
+def notify(type, msg)
+  if $growl
+    $growl.notify "autosync4mac", type, msg, 2, true
+  end
+end
+
+def upload(source, destination, recursive=true)
+  begin
+    $scp.upload!(source, destination, :recursive => recursive)
+    p "#{source} modified, synced to server!"
+  rescue
+    $scp = Net::SCP.start($host, $username, :password => $password)
+    $scp.upload!(source, destination, :recursive => recursive)
+    p "#{source} modified, synced to server!"
+  end
+
+  begin
+    #config growl listen to network connection
+    notify "File sync", "#{source} modified, synced to server!"
+  rescue
+    p $!
+  end
+end
+
+File.open(LOCK_FILE, 'w').close
+
+Kernel.at_exit do
+  p "will delete lock file, exit."
+  File.delete LOCK_FILE
+  notify "error", "sync process exited!"
+end
+
+p "start listen for changes..."
+Listen.to($sco_src_dir) do |modified, added, removed|
+  modified.concat(added).each do |candidate|
+    unless /\.(git|svn|hg)/ =~ candidate
+      upload candidate, $sco_dest_dir + candidate.split($sco_src_dir)[1], File.directory?(candidate)
+    end
+  end
+end
